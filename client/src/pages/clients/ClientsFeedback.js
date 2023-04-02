29117219a0473825d79599590d876659dfc893a6
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2023-04-02 21:42:43 +0300

    chore: assigned site visit and client feedback pg

--- client/src/pages/clients/ClientsFeedback.js
+++ client/src/pages/clients/ClientsFeedback.js
@@ -1,10 +1,133 @@
 import React from "react";
 import Sidebar from "../../components/Sidebar";
 
+const feedbackData = [
+  {
+    id: 1,
+    name: "John Doe",
+    phoneNumber: "0700123456",
+    marketer: "Jane Smith",
+    siteVisited: "ABC Site",
+    dateVisited: "2022-04-01",
+    likedSite: "Yes",
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    phoneNumber: "0711123456",
+    marketer: "John Smith",
+    siteVisited: "XYZ Site",
+    dateVisited: "2022-03-28",
+    likedSite: "No",
+  },
+  {
+    id: 3,
+    name: "Mary Johnson",
+    phoneNumber: "0722123456",
+    marketer: "Peter Parker",
+    siteVisited: "LMN Site",
+    dateVisited: "2022-03-25",
+    likedSite: "Yes",
+  },
+  {
+    id: 4,
+    name: "Tom Smith",
+    phoneNumber: "0733123456",
+    marketer: "Sara Jones",
+    siteVisited: "DEF Site",
+    dateVisited: "2022-03-20",
+    likedSite: "No",
+  },
+  {
+    id: 5,
+    name: "David Brown",
+    phoneNumber: "0744123456",
+    marketer: "Emma Stone",
+    siteVisited: "GHI Site",
+    dateVisited: "2022-03-15",
+    likedSite: "Yes",
+  },
+  {
+    id: 6,
+    name: "Sarah Lee",
+    phoneNumber: "0755123456",
+    marketer: "Tom Johnson",
+    siteVisited: "PQR Site",
+    dateVisited: "2022-03-10",
+    likedSite: "No",
+  },
+  {
+    id: 7,
+    name: "Mike Davis",
+    phoneNumber: "0766123456",
+    marketer: "Anna Brown",
+    siteVisited: "STU Site",
+    dateVisited: "2022-03-05",
+    likedSite: "Yes",
+  },
+  {
+    id: 8,
+    name: "Olivia White",
+    phoneNumber: "0777123456",
+    marketer: "Bob Green",
+    siteVisited: "VWX Site",
+    dateVisited: "2022-03-01",
+    likedSite: "No",
+  },
+  {
+    id: 9,
+    name: "Chris Harris",
+    phoneNumber: "0788123456",
+    marketer: "Karen Lewis",
+    siteVisited: "YZA Site",
+    dateVisited: "2022-02-25",
+    likedSite: "Yes",
+  },
+  {
+    id: 10,
+    name: "Kim Kim",
+    phoneNumber: "0799123456",
+    marketer: "Jerry Lee",
+    siteVisited: "BCD Site",
+    dateVisited: "2022-02-20",
+    likedSite: "No",
+  }
+];
+
 const ClientsFeedback = () => {
   return (
     <>
-      <Sidebar>Clients Feedback</Sidebar>
+      <Sidebar>
+        <div className="container px-4 py-6 mx-auto">
+          <h1 className="text-xl font-bold mb-4">Client Feedback</h1>
+          <div className="overflow-x-auto">
+            <table className="table table-zebra w-full">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Phone Number</th>
+                  <th>Marketer</th>
+                  <th>Site Visited</th>
+                  <th>Date of Visit</th>
+                  <th>Liked Site?</th>
+                </tr>
+              </thead>
+              <tbody>
+                {feedbackData.map((feedback) => (
+                  <tr key={feedback.id}>
+                    <td>{feedback.name}</td>
+                    <td>{feedback.phoneNumber}</td>
+                    <td>{feedback.marketer}</td>
+                    <td>{feedback.siteVisited}</td>
+                    <td>{feedback.dateVisited}</td>
+                    <td>{feedback.likedSite}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </Sidebar>
     </>
   );
 };
--- client/src/pages/site-visit-bookings/AssignedBookings.js
+++ client/src/pages/site-visit-bookings/AssignedBookings.js
@@ -2,11 +2,104 @@ import React from "react";
 import Sidebar from "../../components/Sidebar";
 
 const AssignedBookings = () => {
+  const bookings = [
+    {
+      id: 1,
+      pickupLocation: "123 Main St",
+      siteName: "Site 1",
+      passengers: 4,
+      date: "2023-04-15",
+      time: "10:00 AM",
+    },
+    {
+      id: 2,
+      pickupLocation: "456 Oak St",
+      siteName: "Site 2",
+      passengers: 2,
+      date: "2023-04-16",
+      time: "2:00 PM",
+    },
+    {
+      id: 3,
+      pickupLocation: "789 Elm St",
+      siteName: "Site 3",
+      passengers: 3,
+      date: "2023-03-17",
+      time: "1:00 PM",
+    },
+    {
+      id: 4,
+      pickupLocation: "345 Pine St",
+      siteName: "Site 4",
+      passengers: 6,
+      date: "2023-03-18",
+      time: "9:00 AM",
+    },
+  ];
+
+  const latestBookings = bookings.slice(0, 2); // Show the two latest assigned site visits
+  const pastBookings = bookings.slice(2);
+
   return (
     <>
       <Sidebar>
         <div className="container px-4 py-6 mx-auto">
-          
+          <div className="flex flex-col md:flex-row">
+            <div className="overflow-x-auto md:w-1/2 md:pr-4">
+              <h2 className="text-lg font-bold mb-2">Latest Site Visits</h2>
+              <table className="table table-zebra w-full md:w-3/4">
+                <thead>
+                  <tr>
+                    <th>Site Visit ID</th>
+                    <th>Pickup Location</th>
+                    <th>Site Name</th>
+                    <th>Passengers</th>
+                    <th>Date</th>
+                    <th>Time</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {latestBookings.map((booking) => (
+                    <tr key={booking.id} className="bg-green-200">
+                      <td>{booking.id}</td>
+                      <td>{booking.pickupLocation}</td>
+                      <td>{booking.siteName}</td>
+                      <td>{booking.passengers}</td>
+                      <td>{booking.date}</td>
+                      <td>{booking.time}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+            <div className="overflow-x-auto md:w-1/2 md:pl-4 mt-4 md:mt-0">
+              <h2 className="text-lg font-bold mb-2">Past Site Visits</h2>
+              <table className="table table-zebra w-full md:w-3/4">
+                <thead>
+                  <tr>
+                    <th>Site Visit ID</th>
+                    <th>Pickup Location</th>
+                    <th>Site Name</th>
+                    <th>Passengers</th>
+                    <th>Date</th>
+                    <th>Time</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {pastBookings.map((booking) => (
+                    <tr key={booking.id}>
+                      <td>{booking.id}</td>
+                      <td>{booking.pickupLocation}</td>
+                      <td>{booking.siteName}</td>
+                      <td>{booking.passengers}</td>
+                      <td>{booking.date}</td>
+                      <td>{booking.time}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </Sidebar>
     </>
