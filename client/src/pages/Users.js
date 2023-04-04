cf5521bef1a59a0ab68cb7e5479787fbc0af9a6a
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2023-04-04 11:05:57 +0300

    refactor: users table

--- client/src/pages/Users.js
+++ client/src/pages/Users.js
@@ -1,167 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "../components/Sidebar";
 
 const Users = () => {
+  const [searchText, setSearchText] = useState("");
+  
+  const users = [
+    {
+      id: 1,
+      name: "Kamau Njoroge",
+      role: "Marketer",
+    },
+    {
+      id: 2,
+      name: "Nekesa Wamalwa",
+      role: "Data Analyst",
+    },
+    {
+      id: 3,
+      name: "Mutua Kaluki",
+      role: "Head of Logistics",
+    },
+    {
+      id: 4,
+      name: "Nyambura Kimani",
+      role: "Marketer",
+    },
+    {
+      id: 5,
+      name: "Kerubo Nyakundi",
+      role: "GM",
+    },
+    {
+      id: 6,
+      name: "Kinyanjui Wainaina",
+      role: "Marketer",
+    },
+    {
+      id: 7,
+      name: "Adhiambo Ochieng",
+      role: "Director",
+    },
+    {
+      id: 8,
+      name: "Njoki Gichuru",
+      role: "Marketer",
+    },
+    {
+      id: 9,
+      name: "Onyango Omondi",
+      role: "HOS",
+    },
+    {
+      id: 10,
+      name: "Sara Müller",
+      role: "Marketing Manager",
+    },
+  ];
+
+  const filteredUsers = users.filter((user) =>
+    user.name.toLowerCase().includes(searchText.toLowerCase())
+  );
+
   return (
     <Sidebar>
-      <div className="flex justify-center items-center mt-4 mb-2">
-        <input
-          placeholder="Search User by Name"
-          className="input input-bordered w-full max-w-xs"
-          type="text"
-        />
-      </div>
-      <div className="m-4">
-        <div className="overflow-x-auto">
-          <div className="flex justify-center">
-            <table className="table table-zebra w-full">
-              {/* head */}
-              <thead>
-                <tr>
-                  <th></th>
-                  <th>Name</th>
-                  <th>Role</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {/* row 1 */}
-                <tr>
-                  <th>1</th>
-                  <td>Kamau Njoroge</td>
-                  <td>Marketer</td>
+      <div className="container px-4 py-6 mx-auto">
+        <div className="flex justify-center items-center mb-4 mt-2">
+          <input
+            placeholder="Search User by Name"
+            className="input input-bordered w-full max-w-xs"
+            type="text"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+        </div>
+        <div className="overflow-x-auto card bg-base-100 shadow-xl">
+          <table className="table table-zebra w-full">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Role</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredUsers.map((user) => (
+                <tr key={user.id}>
+                  <td>{user.id}</td>
+                  <td>{user.name}</td>
+                  <td>{user.role}</td>
                   <td>
-                    <button className="btn btn-outline btn-warning">
-                      Edit
-                    </button>
-                    <button className="btn btn-error text-white ml-1">
+                    <button className="btn btn-sm btn-outline btn-warning">Edit</button>
+                    <button className="btn btn-sm btn-error text-white ml-1">
                       Delete
                     </button>
                   </td>
                 </tr>
-                {/* row 2 */}
-                <tr>
-                  <th>2</th>
-                  <td>Nekesa Wamalwa</td>
-                  <td>Data Analyst</td>
-                  <td>
-                    <button className="btn btn-outline btn-warning">
-                      Edit
-                    </button>
-                    <button className="btn btn-error text-white ml-1">
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-                {/* row 3 */}
-                <tr>
-                  <th>3</th>
-                  <td>Mutua Kaluki</td>
-                  <td>Head of Logistics</td>
-                  <td>
-                    <button className="btn btn-outline btn-warning">
-                      Edit
-                    </button>
-                    <button className="btn btn-error text-white ml-1">
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-                {/* row 4 */}
-                <tr>
-                  <th>4</th>
-                  <td>Nyambura Kimani</td>
-                  <td>Marketer</td>
-                  <td>
-                    <button className="btn btn-outline btn-warning">
-                      Edit
-                    </button>
-                    <button className="btn btn-error text-white ml-1">
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-                {/* row 5 */}
-                <tr>
-                  <th>5</th>
-                  <td>Kerubo Nyakundi</td>
-                  <td>GM</td>
-                  <td>
-                    <button className="btn btn-outline btn-warning">
-                      Edit
-                    </button>
-                    <button className="btn btn-error text-white ml-1">
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-                {/* row 6 */}
-                <tr>
-                  <th>6</th>
-                  <td>Kinyanjui Wainaina</td>
-                  <td>Marketer</td>
-                  <td>
-                    <button className="btn btn-outline btn-warning">
-                      Edit
-                    </button>
-                    <button className="btn btn-error text-white ml-1">
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-                {/* row 7 */}
-                <tr>
-                  <th>7</th>
-                  <td>Adhiambo Ochieng</td>
-                  <td>Director</td>
-                  <td>
-                    <button className="btn btn-outline btn-warning">
-                      Edit
-                    </button>
-                    <button className="btn btn-error text-white ml-1">
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-                {/* row 8 */}
-                <tr>
-                  <th>8</th>
-                  <td>Njoki Gichuru</td>
-                  <td>Marketer</td>
-                  <td>
-                    <button className="btn btn-outline btn-warning">
-                      Edit
-                    </button>
-                    <button className="btn btn-error text-white ml-1">
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-                {/* row 9 */}
-                <tr>
-                  <th>9</th>
-                  <td>Onyango Omondi</td>
-                  <td>HOS</td>
-                  <td>
-                    <button className="btn btn-outline btn-warning">
-                      Edit
-                    </button>
-                    <button className="btn btn-error text-white ml-1">
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              </tbody>
-              <tfoot>
-                <tr>
-                  <th></th>
-                  <th>Name</th>
-                  <th>Role</th>
-                  <th>Action</th>
-                </tr>
-              </tfoot>
-            </table>
-          </div>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </Sidebar>
