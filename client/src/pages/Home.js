2e2de2aca1d271632c02422932745853a1e438b0
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2023-05-08 16:23:37 +0300

    fix: rm popup

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,19 +1,11 @@
-import React, { useEffect, useState } from "react";
 import Sidebar from "../components/Sidebar";
 import { useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import bus from "../assets/home.jpg";
-import Popup from "../components/Popup";
-import {
-  completeSiteVisit,
-  selectActiveSiteVisits,
-} from "../redux/features/siteVisit/siteVisitSlice";
+import { selectActiveSiteVisits } from "../redux/features/siteVisit/siteVisitSlice";
 
 const Home = () => {
-  const [showPopup, setShowPopup] = useState(false);
-
   const activeSiteVisits = useSelector(selectActiveSiteVisits);
-  const dispatch = useDispatch();
 
   const user = JSON.parse(localStorage.getItem("user")) || {};
   const firstName = user.fullnames
@@ -42,69 +34,44 @@ const Home = () => {
     accessRole === `    112#770#303#304#305#116` ||
     accessRole === `     112#114#700`;
 
-  const handleCompleteClick = () => {
-    navigate("/survey");
-    setShowPopup(false);
-  };
-
-  useEffect(() => {
-    console.log("Active site visits:", activeSiteVisits);
-    const completedVisit = activeSiteVisits.find(
-      (visit) => visit.status === "complete"
-    );
-
-    if (completedVisit && !showPopup) {
-      dispatch(completeSiteVisit({ id: completedVisit.id }));
-      setShowPopup(true);
-    }
-  }, [activeSiteVisits, dispatch]);
-
   return (
     <>
       <Sidebar>
-        {showPopup && (
-          <div className="fixed inset-0 flex items-center justify-center z-50">
-            <div className="bg-black opacity-50 w-full h-full" />
-            <Popup onCompleteClick={handleCompleteClick} />
-          </div>
-        )}
-        <div className={`${showPopup ? "opacity-50 pointer-events-none" : ""}`}>
-          <div className="flex justify-center items-center mt-20">
-            <div className="w-2/3">
-              <div className="overflow-hidden rounded-lg shadow-2xl md:grid md:grid-cols-3">
-                <img
-                  alt="Bus"
-                  src={bus}
-                  className="h-32 w-full object-cover md:h-full"
-                />
+        <div className="flex justify-center items-center mt-20">
+          <div className="w-2/3">
+            <div className="overflow-hidden rounded-lg shadow-2xl md:grid md:grid-cols-3">
+              <img
+                alt="Bus"
+                src={bus}
+                className="h-32 w-full object-cover md:h-full"
+              />
 
-                <div className="flex flex-col justify-center p-4 text-start sm:p-6 md:col-span-2 lg:p-8">
-                  <h2 className="mt-6 font-black">
-                    <span className="text-4xl sm:text-5xl lg:text-7xl">
-                      {greeting} {firstName}.
-                    </span>
-                  </h2>
-                  {(isMarketer || isAdmin) && (
-                    <button
-                      onClick={() => navigate("/book-site-visit")}
-                      disabled={activeSiteVisits.length > 0}
-                      className={`mt-8 inline-block w-full bg-primary py-4 text-sm font-bold uppercase tracking-widest text-white ${
-                        activeSiteVisits.length > 0
-                          ? "opacity-50 cursor-not-allowed"
-                          : ""
-                      }`}
-                    >
-                      Book a Site Visit
-                    </button>
-                  )}
-
+              <div className="flex flex-col justify-center p-4 text-start sm:p-6 md:col-span-2 lg:p-8">
+                <h2 className="mt-6 font-black">
+                  <span className="text-4xl sm:text-5xl lg:text-7xl">
+                    {greeting} {firstName}.
+                  </span>
+                </h2>
+                {(isMarketer || isAdmin) && (
                   <button
-                    onClick={() => navigate("/request-vehicle")}
-                    className="mt-4 inline-block w-full bg-neutral py-4 text-sm font-bold uppercase tracking-widest text-white"
+                    onClick={() => navigate("/book-site-visit")}
+                    disabled={activeSiteVisits.length > 0}
+                    className={`mt-8 inline-block w-full bg-primary py-4 text-sm font-bold uppercase tracking-widest text-white ${
+                      activeSiteVisits.length > 0
+                        ? "opacity-50 cursor-not-allowed"
+                        : ""
+                    }`}
                   >
-                    Request for a Vehicle
+                    Book a Site Visit
                   </button>
-                </div>
+                )}
+
+                <button
+                  onClick={() => navigate("/request-vehicle")}
+                  className="mt-4 inline-block w-full bg-neutral py-4 text-sm font-bold uppercase tracking-widest text-white"
+                >
+                  Request for a Vehicle
+                </button>
               </div>
             </div>
           </div>
