c37d19be7b912acb22563361e49945fec5186c2e
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2023-05-12 11:37:22 +0300

    feat: add sidebar to survey page

--- client/src/pages/Survey.js
+++ client/src/pages/Survey.js
@@ -3,6 +3,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Sidebar from "../components/Sidebar";
 
 const Survey = () => {
   const [visited, setVisited] = useState("");
@@ -82,107 +83,112 @@ const Survey = () => {
   };
 
   return (
-    <section className="flex justify-center items-center mt-10">
-      <div className=" w-full max-w-xs">
-        <div>
-          <h1 className="font-extrabold text-3xl uppercase text-center">
-            Feedback
-          </h1>
-        </div>
-        <form onSubmit={handleSubmit} className="mx-auto mb-0 mt-8 space-y-4">
+    <Sidebar>
+      <section className="flex justify-center items-center mt-10">
+        <div className=" w-full max-w-xs">
           <div>
-            <label htmlFor="clientVisit" className="label text-md">
-              Did the client visit the site?
-            </label>
-            <div className="flex justify-evenly lg:justify-between">
-              <select
-                className="select select-bordered w-full max-w-xs"
-                value={visited}
-                onChange={handleVisitedChange}
-              >
-                <option value="">Please select</option>
-                <option value="Yes">Yes</option>
-                <option value="No">No</option>
-              </select>
-            </div>
-            {visited === "Yes" && (
-              <div>
-                <label htmlFor="bookedPlot" className="label text-md">
-                  Did the client book the plot?
-                </label>
+            <h1 className="font-extrabold text-3xl uppercase text-center">
+              Feedback
+            </h1>
+          </div>
+          <form onSubmit={handleSubmit} className="mx-auto mb-0 mt-8 space-y-4">
+            <div>
+              <label htmlFor="clientVisit" className="label text-md">
+                Did the client visit the site?
+              </label>
+              <div className="flex justify-evenly lg:justify-between">
                 <select
                   className="select select-bordered w-full max-w-xs"
-                  value={booked}
-                  onChange={handleBookedChange}
+                  value={visited}
+                  onChange={handleVisitedChange}
                 >
                   <option value="">Please select</option>
                   <option value="Yes">Yes</option>
                   <option value="No">No</option>
                 </select>
-                {booked === "Yes" && (
-                  <div>
-                    <label htmlFor="amountReserved" className="label text-md">
-                      How much money was reserved in KES?
-                    </label>
-                    <input
-                      type="number"
-                      id="amountReserved"
-                      value={amountReserved}
-                      onChange={(e) => setAmountReserved(e.target.value)}
-                      className="input input-bordered w-full max-w-xs"
-                      placeholder="100000"
-                    />
-                    <label htmlFor="plotDetails" className="label text-md">
-                      What are the details of the plot that they reserved?
-                    </label>
-                    <textarea
-                      type="text"
-                      id="plotDetails"
-                      value={plotDetails}
-                      onChange={(e) => setPlotDetails(e.target.value)}
-                      className="textarea textarea-bordered h-24 w-full max-w-xs"
-                    />
-                  </div>
-                )}
-                {booked === "No" && (
-                  <div>
-                    <label htmlFor="reasonNotBooked" className="label text-md">
-                      Why did the client NOT book a plot?
-                    </label>
-                    <textarea
-                      type="text"
-                      id="reasonNotBooked"
-                      value={reasonNotBooked}
-                      onChange={(e) => setReasonNotBooked(e.target.value)}
-                      className="textarea textarea-bordered w-full max-w-xs"
-                    />
-                  </div>
-                )}
               </div>
-            )}
-            {visited === "No" && (
-              <div>
-                <label htmlFor="reasonNoVisit" className="label text-md">
-                  Why did the client not visit the site?
-                </label>
-                <textarea
-                  id="reasonNoVisit"
-                  value={reasonNoVisit}
-                  onChange={(e) => setReasonNoVisit(e.target.value)}
-                  className="textarea textarea-bordered h-24 w-full max-w-xs"
-                />
-              </div>
-            )}
-            <button
-              className="btn btn-primary w-full max-w-xs mt-2 text-white"
-              type="submit"
-            >
-              Submit
-            </button>
-          </div>
-        </form>
-      </div>
-    </section>
+              {visited === "Yes" && (
+                <div>
+                  <label htmlFor="bookedPlot" className="label text-md">
+                    Did the client book the plot?
+                  </label>
+                  <select
+                    className="select select-bordered w-full max-w-xs"
+                    value={booked}
+                    onChange={handleBookedChange}
+                  >
+                    <option value="">Please select</option>
+                    <option value="Yes">Yes</option>
+                    <option value="No">No</option>
+                  </select>
+                  {booked === "Yes" && (
+                    <div>
+                      <label htmlFor="amountReserved" className="label text-md">
+                        How much money was reserved in KES?
+                      </label>
+                      <input
+                        type="number"
+                        id="amountReserved"
+                        value={amountReserved}
+                        onChange={(e) => setAmountReserved(e.target.value)}
+                        className="input input-bordered w-full max-w-xs"
+                        placeholder="100000"
+                      />
+                      <label htmlFor="plotDetails" className="label text-md">
+                        What are the details of the plot that they reserved?
+                      </label>
+                      <textarea
+                        type="text"
+                        id="plotDetails"
+                        value={plotDetails}
+                        onChange={(e) => setPlotDetails(e.target.value)}
+                        className="textarea textarea-bordered h-24 w-full max-w-xs"
+                      />
+                    </div>
+                  )}
+                  {booked === "No" && (
+                    <div>
+                      <label
+                        htmlFor="reasonNotBooked"
+                        className="label text-md"
+                      >
+                        Why did the client NOT book a plot?
+                      </label>
+                      <textarea
+                        type="text"
+                        id="reasonNotBooked"
+                        value={reasonNotBooked}
+                        onChange={(e) => setReasonNotBooked(e.target.value)}
+                        className="textarea textarea-bordered w-full max-w-xs"
+                      />
+                    </div>
+                  )}
+                </div>
+              )}
+              {visited === "No" && (
+                <div>
+                  <label htmlFor="reasonNoVisit" className="label text-md">
+                    Why did the client not visit the site?
+                  </label>
+                  <textarea
+                    id="reasonNoVisit"
+                    value={reasonNoVisit}
+                    onChange={(e) => setReasonNoVisit(e.target.value)}
+                    className="textarea textarea-bordered h-24 w-full max-w-xs"
+                  />
+                </div>
+              )}
+              <button
+                className="btn btn-primary w-full max-w-xs mt-2 text-white"
+                type="submit"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </Sidebar>
   );
 };
 
