247347deb4e7cf422c720fa5761bf676dda97f4b
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2023-06-30 15:54:03 +0300

    chore: implement additional fields in visitor info

--- client/src/visitors-management/pages/VisitorsManagementHome.js
+++ client/src/visitors-management/pages/VisitorsManagementHome.js
@@ -10,7 +10,7 @@ const VisitorsManagementHome = () => {
           <aside className="relative block h-96 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6">
             <img
               alt="Pattern"
-              src="https://images.unsplash.com/photo-1605106702734-205df224ecce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+              src="https://images.unsplash.com/photo-1638184984605-af1f05249a56?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1032&q=80"
               className="absolute inset-0 h-full w-full object-cover"
             />
           </aside>
