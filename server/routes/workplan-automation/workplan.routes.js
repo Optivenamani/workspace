fb0a18722657efbb46ed590be91c60ee43742717
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2023-09-11 14:37:52 +0300

    chore: add email msg

--- server/routes/workplan-automation/workplan.routes.js
+++ server/routes/workplan-automation/workplan.routes.js
@@ -4,30 +4,27 @@ const checkPermissions = require("../../middleware/checkPermissions");
 const router = express.Router();
 const nodemailer = require("nodemailer");
 
-
-
 // Nodemailer helper function to send email
 async function sendEmail(userEmail, subject, text) {
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
     host: "smtp.zoho.com",
     port: 465,
-    secure: true, 
+    secure: true,
     auth: {
-      user: `notify@optiven.co.ke`, 
-      pass: `Peace@6t4r#!`, 
+      user: `notify@optiven.co.ke`,
+      pass: `Peace@6t4r#!`,
     },
   });
 
   // send mail with defined transport object
   let info = await transporter.sendMail({
-    from: '"Optiven Workplan 💂" <notify@optiven.co.ke>', 
-    to: userEmail, 
-    subject: subject, 
-    text: text, 
+    from: '"Optiven Workplan 💂" <notify@optiven.co.ke>',
+    to: userEmail,
+    subject: subject,
+    text: text,
   });
 }
-
 
 module.exports = (pool) => {
   // GET all workplans for the authenticated user
@@ -75,7 +72,7 @@ module.exports = (pool) => {
   });
 
   // GET a specific workplan
-  router.get("/:id",  (req, res) => {
+  router.get("/:id", (req, res) => {
     const { id } = req.params;
     const query = "SELECT * FROM workplans WHERE id = ?";
     pool.query(query, [id], (err, workplan) => {
@@ -201,155 +198,151 @@ module.exports = (pool) => {
     });
   });
 
-// Route to approve a workplan (only accessible by authorized Level 1 Managers)
-router.put("/:id/approve", (req, res) => {
-  const { id } = req.params;
-  const query = "UPDATE workplans SET status = 'approved' WHERE id = ?";
-  
-  // Fetch the marketerid from the workplans table
-  const getMarketerIdQuery = "SELECT marketer_id FROM workplans WHERE id = ?";
-  pool.query(getMarketerIdQuery, [id], async (err, marketerResult) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ message: "Server Error" });
-      return;
-    }
+  // Route to approve a workplan (only accessible by authorized Level 1 Managers)
+  router.put("/:id/approve", (req, res) => {
+    const { id } = req.params;
+    const query = "UPDATE workplans SET status = 'approved' WHERE id = ?";
 
-    if (marketerResult.length === 0) {
-      res.status(404).json({ message: "Workplan not found" });
-      return;
-    }
-
-    const marketerId = marketerResult[0].marketer_id;
-    
-
-    // Now, use the marketerId as user_id to fetch the user's email and full name from the defaultdb.users table
-    const getEmailAndFullNameQuery = "SELECT email, fullnames FROM defaultdb.users WHERE user_id = ?";
-    pool.query(getEmailAndFullNameQuery, [marketerId], async (userErr, userResult) => {
-      if (userErr) {
-        console.error(userErr);
+    // Fetch the marketerid from the workplans table
+    const getMarketerIdQuery = "SELECT marketer_id FROM workplans WHERE id = ?";
+    pool.query(getMarketerIdQuery, [id], async (err, marketerResult) => {
+      if (err) {
+        console.error(err);
         res.status(500).json({ message: "Server Error" });
         return;
       }
 
-      if (userResult.length === 0) {
-        console.log("User not found");
-        res.status(404).json({ message: "Recipient not found" });
+      if (marketerResult.length === 0) {
+        res.status(404).json({ message: "Workplan not found" });
         return;
       }
 
-      const userEmail = userResult[0].email;
-      const userFullName = userResult[0].fullnames;
+      const marketerId = marketerResult[0].marketer_id;
 
-      
+      // Now, use the marketerId as user_id to fetch the user's email and full name from the defaultdb.users table
+      const getEmailAndFullNameQuery =
+        "SELECT email, fullnames FROM defaultdb.users WHERE user_id = ?";
+      pool.query(
+        getEmailAndFullNameQuery,
+        [marketerId],
+        async (userErr, userResult) => {
+          if (userErr) {
+            console.error(userErr);
+            res.status(500).json({ message: "Server Error" });
+            return;
+          }
 
-      // Now, update the workplan status
-      pool.query(query, [id], async (updateErr, updateResult) => {
-        if (updateErr) {
-          console.error(updateErr);
-          res.status(500).json({ message: "Server Error" });
-        } else if (updateResult.affectedRows > 0) {
-          // Send approval email notification with recipient's full name
-          const subject = "Workplan Approved";
-          const text = `
+          if (userResult.length === 0) {
+            console.log("User not found");
+            res.status(404).json({ message: "Recipient not found" });
+            return;
+          }
+
+          const userEmail = userResult[0].email;
+          const userFullName = userResult[0].fullnames;
+
+          // Now, update the workplan status
+          pool.query(query, [id], async (updateErr, updateResult) => {
+            if (updateErr) {
+              console.error(updateErr);
+              res.status(500).json({ message: "Server Error" });
+            } else if (updateResult.affectedRows > 0) {
+              // Send approval email notification with recipient's full name
+              const subject = "Workplan Approved";
+              const text = `
             Dear ${userFullName},
 
-            Your workplan has been approved successfully. Here are the details:
-
-            - Workplan ID: ${id}
-            - Status: Approved
+            Your workplan has been approved successfully. You may proceed to update it with the outcome of the activites you shall be handling all week.
 
             Thank you for your hard work!
 
             Sincerely,
-            Optiven Team
+            Optiven Work Plan
           `;
-          await sendEmail(userEmail, subject, text);
+              await sendEmail(userEmail, subject, text);
 
-          res.json({ message: "Workplan approved successfully" });
-        } else {
-          res.status(404).json({ message: "Workplan not found" });
+              res.json({ message: "Workplan approved successfully" });
+            } else {
+              res.status(404).json({ message: "Workplan not found" });
+            }
+          });
         }
-      });
+      );
     });
   });
-});
 
+  // Route to reject a workplan (only accessible by authorized Level 1 Managers)
+  router.put("/:id/reject", (req, res) => {
+    const { id } = req.params;
+    const query = "UPDATE workplans SET status = 'rejected' WHERE id = ?";
 
-
-// Route to reject a workplan (only accessible by authorized Level 1 Managers)
-router.put("/:id/reject", (req, res) => {
-  const { id } = req.params;
-  const query = "UPDATE workplans SET status = 'rejected' WHERE id = ?";
-
-  // Fetch the marketer_id from the workplans table
-  const getMarketerIdQuery = "SELECT marketer_id FROM workplans WHERE id = ?";
-  pool.query(getMarketerIdQuery, [id], async (err, marketerResult) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ message: "Server Error" });
-      return;
-    }
-
-    if (marketerResult.length === 0) {
-      res.status(404).json({ message: "Workplan not found" });
-      return;
-    }
-
-    const marketerId = marketerResult[0].marketer_id;
-
-    // Now, use the marketerId as user_id to fetch the user's email and full name from the defaultdb.users table
-    const getEmailAndFullNameQuery = "SELECT email, fullnames FROM defaultdb.users WHERE user_id = ?";
-    pool.query(getEmailAndFullNameQuery, [marketerId], async (userErr, userResult) => {
-      if (userErr) {
-        console.error(userErr);
+    // Fetch the marketer_id from the workplans table
+    const getMarketerIdQuery = "SELECT marketer_id FROM workplans WHERE id = ?";
+    pool.query(getMarketerIdQuery, [id], async (err, marketerResult) => {
+      if (err) {
+        console.error(err);
         res.status(500).json({ message: "Server Error" });
         return;
       }
 
-      if (userResult.length === 0) {
-        console.log("User not found");
-        res.status(404).json({ message: "Recipient not found" });
+      if (marketerResult.length === 0) {
+        res.status(404).json({ message: "Workplan not found" });
         return;
       }
 
-      const userEmail = userResult[0].email;
-      const userFullName = userResult[0].fullnames;
+      const marketerId = marketerResult[0].marketer_id;
 
-      // Now, update the workplan status
-      pool.query(query, [id], async (updateErr, updateResult) => {
-        if (updateErr) {
-          console.error(updateErr);
-          res.status(500).json({ message: "Server Error" });
-        } else if (updateResult.affectedRows > 0) {
-          // Send rejection email notification with recipient's full name
-          const subject = "Workplan Rejected";
-          const text = `
+      // Now, use the marketerId as user_id to fetch the user's email and full name from the defaultdb.users table
+      const getEmailAndFullNameQuery =
+        "SELECT email, fullnames FROM defaultdb.users WHERE user_id = ?";
+      pool.query(
+        getEmailAndFullNameQuery,
+        [marketerId],
+        async (userErr, userResult) => {
+          if (userErr) {
+            console.error(userErr);
+            res.status(500).json({ message: "Server Error" });
+            return;
+          }
+
+          if (userResult.length === 0) {
+            console.log("User not found");
+            res.status(404).json({ message: "Recipient not found" });
+            return;
+          }
+
+          const userEmail = userResult[0].email;
+          const userFullName = userResult[0].fullnames;
+
+          // Now, update the workplan status
+          pool.query(query, [id], async (updateErr, updateResult) => {
+            if (updateErr) {
+              console.error(updateErr);
+              res.status(500).json({ message: "Server Error" });
+            } else if (updateResult.affectedRows > 0) {
+              // Send rejection email notification with recipient's full name
+              const subject = "Workplan Rejected";
+              const text = `
             Dear ${userFullName},
 
-            Unfortunately, your workplan has been rejected. Here are the details:
-
-            - Workplan ID: ${id}
-            - Status: Rejected
+            Unfortunately, your workplan has been rejected. Get in touch with your regional manager to get to know why.
 
             If you have any questions or need further assistance, please don't hesitate to reach out to us.
 
             Sincerely,
-            Optiven Team
+            Optiven Work Plan
           `;
-          await sendEmail(userEmail, subject, text);
+              await sendEmail(userEmail, subject, text);
 
-          res.json({ message: "Workplan rejected successfully" });
-        } else {
-          res.status(404).json({ message: "Workplan not found" });
+              res.json({ message: "Workplan rejected successfully" });
+            } else {
+              res.status(404).json({ message: "Workplan not found" });
+            }
+          });
         }
-      });
+      );
     });
   });
-});
-
-
-
 
   // DELETE a workplan for the authenticated user
   router.delete("/:id", authenticateJWT, (req, res) => {
