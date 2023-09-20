c427df0b5ee9fbe34047ca41575c760cd653be1f
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2023-09-20 14:55:35 +0300

    chore: profile boilerplate

--- client/src/common/pages/Profile.js
+++ client/src/common/pages/Profile.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
   const token = useSelector((state) => state.user.token);
@@ -92,11 +93,11 @@ const Profile = () => {
 
   return (
     <section className="text-gray-600 body-font">
-      <div className="container px-5 py-24 mx-auto flex flex-col">
-        <div className="flex flex-col">
-          <div className="text-center">
-            <div className="flex flex-col items-center text-center justify-center">
-              <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
+      <div className="container px-5 py-10 mx-auto flex flex-col">
+        <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 lg:gap-8">
+          <div className="h-full rounded-lg bg-gray-100 pb-10">
+            <div className="flex justify-center items-center mt-6">
+              <div className="w-15 h-15 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400 border-gray-500 p-2 border-4">
                 <svg
                   fill="none"
                   stroke="currentColor"
@@ -110,27 +111,42 @@ const Profile = () => {
                   <circle cx={12} cy={7} r={4} />
                 </svg>
               </div>
-              <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">
-                {user.fullnames}
-              </h2>
-              <div className="w-12 h-1 bg-green-500 rounded mt-2 mb-4" />
-              <ul>
-                <li className="flex items-center">
+              <div>
+                <h2 className="text-md font-bold ml-2">{user.fullnames}</h2>
+                <h2 className="text-xs italic ml-2">{user.email}</h2>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center mt-6">
+              <div className="flex justify-between">
+                <div className="flex items-center justify-between">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width={16}
                     height={16}
                     fill="currentColor"
-                    className="bi bi-envelope mr-2"
+                    className="bi bi-phone mr-2"
                     viewBox="0 0 16 16"
                   >
                     {" "}
-                    <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />{" "}
+                    <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z" />{" "}
+                    <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />{" "}
                   </svg>
-                  <div className="font-bold text-sm italic">{user.email}</div>
-                </li>
-                <div className="flex justify-between">
-                  <li className="flex items-center justify-between">
+                  <div className="font-bold text-sm italic mr-2">
+                    {user.phone_number
+                      ? "+" + user.phone_number
+                      : "Missing phone number"}
+                  </div>
+                  <button
+                    className="btn btn-xs text-white"
+                    onClick={() => handleView()}
+                  >
+                    Edit
+                  </button>
+                </div>
+              </div>
+              {user.team && (
+                <div className="flex justify-start ">
+                  <div className="flex items-center justify-between">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width={16}
@@ -144,36 +160,76 @@ const Profile = () => {
                       <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />{" "}
                     </svg>
                     <div className="font-bold text-sm italic mr-2">
-                      {user.phone_number ? user.phone_number : "Missing phone number"}
+                      {user.team}
                     </div>
-                    <button
-                      className="btn btn-secondary btn-xs text-white"
-                      onClick={() => handleView()}
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width={16}
-                        height={16}
-                        fill="currentColor"
-                        className="bi bi-pencil-square mr-2"
-                        viewBox="0 0 16 16"
-                      >
-                        {" "}
-                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />{" "}
-                        <path
-                          fillRule="evenodd"
-                          d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
-                        />{" "}
-                      </svg>
-                      Edit
-                    </button>
-                  </li>
+                  </div>
                 </div>
-              </ul>
+              )}
+            </div>
+          </div>
+          <div className="h-full rounded-lg bg-gray-100 py-10 flex flex-col items-center lg:h-1/2">
+            <div className="flex flex-col justify-center">
+              <Link to="/" className="font-bold hover:underline">Menu</Link>
+              <Link to="/feedback" className="font-bold hover:underline">Feedback</Link>
+              <Link to="/login" className="font-bold hover:underline">Logout</Link>
+            </div>
+          </div>
+          <div className="h-full rounded-lg bg-gray-100 lg:col-span-2">
+            <div className="flex justify-center items-center my-6 mx-6 lg:justify-start">
+              <div className="w-40 h-40 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400 border-gray-500 p-2 border-4">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  className="w-20 h-20"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
+                  <circle cx={12} cy={7} r={4} />
+                </svg>
+              </div>
+            </div>
+            <div className="flex justify-center items-center m-6 lg:justify-start">
+              <div className="flex flex-col items-center justify-center">
+                <div
+                  className="radial-progress text-primary border-4 mx-2"
+                  style={{ "--value": 95 }}
+                >
+                  95%
+                </div>
+                <p className="w-1/2 text-sm text-center">
+                  95th percentile of something
+                </p>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <div
+                  className="radial-progress text-warning border-4 mx-2"
+                  style={{ "--value": 75 }}
+                >
+                  75%
+                </div>
+                <p className="w-1/2 text-sm text-center">
+                  75th percentile of something
+                </p>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <div
+                  className="radial-progress text-error border-4 mx-2"
+                  style={{ "--value": 25 }}
+                >
+                  25%
+                </div>
+                <p className="w-1/2 text-sm text-center">
+                  25th percentile of something
+                </p>
+              </div>
             </div>
           </div>
         </div>
       </div>
+
       {/* Modal for editing phone number */}
       {modalOpened && (
         <div className="fixed inset-0 flex justify-center items-center z-10 bg-black bg-opacity-50">
