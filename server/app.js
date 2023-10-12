aa848129ca3d3f85f6a416f7bfaa929ee2116d8a
JemmyKibandi <jemimakibandi378@gmail.com>
2023-10-12 13:22:26 +0300

    chore: publish branch

---
+++ server/app.js
@@ -0,0 +1,11 @@
+// imports and setup
+const express = require('express')
+const app = express()
+
+// routes
+app.get("/", (req, res) => res.send("Hi from the root route"))
+
+// telling the app to listen to this port
+app.listen(6969, () => {
+    console.log("App is running on port 6969")
+})
