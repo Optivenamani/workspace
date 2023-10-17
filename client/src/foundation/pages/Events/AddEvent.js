e91764e3b58aa189db84197fe814e48e33c4fc72
JemmyKibandi <jemimakibandi378@gmail.com>
2023-10-17 14:51:08 +0300

    core: new routes

---
+++ client/src/foundation/pages/Events/AddEvent.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Sidebar from "../../components/Sidebar";
+
+const AddEvent = () => {
+  return (
+    <Sidebar>
+    </Sidebar>
+  );
+};
+
+export default AddEvent;
